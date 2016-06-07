8de894b7cd89b984d3ef4b3a4dbab62caeabea4d
LaTaevia Berry <lataevia.berry@gmail.com>
2016-06-07 16:26:45 -0700

    add html loop into app.component's template.

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,7 +6,7 @@ import { Food } from './food.model';
   template: `
   <div class="container">
     <h1>Meal Tracker App</h1>
-    <h3>{{ food.name }} <br> {{ food.calorie }} <br> {{ food.detail }}</h3>
+    <h3 *ngFor="#food of foods">{{ food.name }} <br> {{ food.calorie }} <br> {{ food.detail }}</h3>
   </div>
   `
 })
