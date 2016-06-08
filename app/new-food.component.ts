3b9fc3b379858f9059a20d1383316bb9226b6fbe
LaTaevia Berry <lataevia.berry@gmail.com>
2016-06-08 09:01:04 -0700

    send an output when the NewFoodComponent form is submitted

--- app/new-food.component.ts
+++ app/new-food.component.ts
@@ -1,13 +1,17 @@
-import {Component} from 'angular2/core';
+import {Component, EventEmitter} from 'angular2/core';
 import {Food} from './food.model';
 
 @Component ({
   selector: 'new-food',
+  outputs: ['onSubmitNewFood'],
   template: `
   <h3>This template is blank for now</h3>
   `
 })
 
 export class NewFoodComponent {
-
+  public onSubmitNewFood: EventEmitter<Food>;
+  constructor(){
+    this.onSubmitNewFood = new EventEmitter();
+  }
 }
