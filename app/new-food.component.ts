32abe32e449814fcab89318c767cb7a2297e0611
LaTaevia Berry <lataevia.berry@gmail.com>
2016-06-08 09:11:18 -0700

    create form and connect output of click EventEmitter to addFood method in add food component

--- app/new-food.component.ts
+++ app/new-food.component.ts
@@ -5,13 +5,29 @@ import {Food} from './food.model';
   selector: 'new-food',
   outputs: ['onSubmitNewFood'],
   template: `
-  <h3>This template is blank for now</h3>
+  <div class="col-sm-8">
+    <h3>Log Food:</h3>
+    <input placeholder="Name of Food" class="input-lg form-control food-form">
+    <input type=number placeholder="Number of Calories" class="input-lg form-control food-form">
+    <input placeholder="Additional Details" class="input-lg form-control food-form">
+    <button (click)="addFood()">Add</button>
+  </div>
   `
 })
+
+//<input [(ngModel)]="food.calorie" class="input-lg form-control food-form"/>
+// <div class="task-form">
+//     <h3>Create Task:</h3>
+//     <input placeholder="Description" class="col-sm-8 input-lg">
+//     <button (click)="addTask()">Add</button>
+//   </div>
 
 export class NewFoodComponent {
   public onSubmitNewFood: EventEmitter<Food>;
   constructor(){
     this.onSubmitNewFood = new EventEmitter();
+  }
+  addFood(){
+    console.log("Loading all the noms...");
   }
 }
