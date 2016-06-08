d6668feac49394544096c13b6990ee78b4d7c38f
LaTaevia Berry <lataevia.berry@gmail.com>
2016-06-07 20:51:55 -0700

    add basic structure for food.component to store a single food object

---
+++ app/food.component.ts
@@ -0,0 +1,14 @@
+import { Component } from 'angular2/core';
+import { Food } from './food.model';
+
+@Component({
+    selector: 'food-display',
+    inputs: ['food'],
+  template: `
+    <h3>{{ food.name }} <br> {{ food.calorie }} <br> {{ food.detail }}</h3>
+  `
+})
+
+export class FoodComponent {
+  public task: Food;
+}
