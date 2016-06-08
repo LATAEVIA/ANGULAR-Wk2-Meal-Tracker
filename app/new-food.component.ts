7e15353dff9c3e5998e58c58705e9f1aaa34cf5f
LaTaevia Berry <lataevia.berry@gmail.com>
2016-06-08 08:51:47 -0700

    add basic structure for New Food Component

---
+++ app/new-food.component.ts
@@ -0,0 +1,13 @@
+import {Component} from 'angular2/core';
+import {Food} from './food.model';
+
+@Component ({
+  selector: 'new-food',
+  template: `
+  <h3>THis template is blank for now</h3>
+  `
+})
+
+export class NewFoodComponent {
+
+}
