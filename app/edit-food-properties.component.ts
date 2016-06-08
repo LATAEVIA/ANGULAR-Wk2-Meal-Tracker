f25d0018cb8b4b6effb714ecf87a2b021922ccd8
LaTaevia Berry <lataevia.berry@gmail.com>
2016-06-07 22:19:00 -0700

    update edit component template to display food properties

--- app/edit-food-properties.component.ts
+++ app/edit-food-properties.component.ts
@@ -5,6 +5,9 @@ import {Food} from './food.model';
   selector: 'edit-food-properties',
   inputs: ['food'],
   template: `
+    <h3>Edit Name: {{ food.name }}</h3>
+    <h3>Edit Calorie Count: {{ food.calorie }}</h3>
+    <h3>Edit Details: {{ food.detail }}</h3>
   `
 })
 export class EditFoodDPorpertiesComponent {
