2377b64a9d00e53123f80b7b67f9097371141fae
LaTaevia Berry <lataevia.berry@gmail.com>
2016-06-07 19:26:07 -0700

    update foodlistcomponent template

--- app/food-list.component.ts
+++ app/food-list.component.ts
@@ -3,7 +3,9 @@ import { Component, EventEmitter } from 'angular2/core';
 @Component({
   selector: 'food-list',
   template: `
-  <h3></h3>
+  <h3 *ngFor="#currentFood of foodList" (click)="foodClicked(currentFood)">
+    {{ food.name }} <br> {{ food.calorie }} <br> {{ food.detail }}
+  </h3>
   `
 })
 
